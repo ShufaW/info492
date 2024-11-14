3c240d5cde2a6e6ad764ae94c84eaeb1413df01d
joseph02 <joseph02@uw.edu>
2024-11-13 22:03:09 -0800

    added reddit

--- reddit.py
+++ reddit.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('./pse_isr_reddit_comments.csv')
-print(df.columns)
+reddit_csv_path = './pse_isr_reddit_comments.csv'
+
+columns = ['self_text', 'created_time']
+
+def keep_columns(csv_file_path, columns_to_keep):
+    """Reads a CSV file and keeps only the specified columns."""
+    df = pd.read_csv(csv_file_path, usecols=columns_to_keep)
+    return df
+
+reddit_cleaned_data = keep_columns(reddit_csv_path, columns)
+
+reddit_cleaned_data.drop_duplicates(inplace=True)
+
+reddit_cleaned_data.to_csv('df', index=False)
